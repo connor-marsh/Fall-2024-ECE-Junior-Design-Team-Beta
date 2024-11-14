929a73deae89a949abbf0bfd8538d4ffeedcb1d8
oas2316 <156560665+oas2316@users.noreply.github.com>
2024-11-14 10:45:47 -0500

    Add files via upload
    Code for DFplayer

---
+++ DFPlayerPC.ino
@@ -0,0 +1,14 @@
+#include "mp3tf16p.h"
+
+MP3Player mp3(10,11);
+
+void setup() {
+  Serial.begin(9600);
+  mp3.initialize();
+  mp3.playTrackNumber(2,30);
+  //mp3.playTrackNumber(3,30);
+}
+
+void loop() {
+  mp3.serialPrintStatus(MP3_ALL_MESSAGE);
+}
